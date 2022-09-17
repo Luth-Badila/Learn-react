f1e2307dd709b11394f6719adb43dd31cd328ef1
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-17 23:10:10 +0700

    make basic vite + react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <div className="flex justify-center items-center h-screen">Welcome to the jungle</div>;
+}
+
+export default App;
