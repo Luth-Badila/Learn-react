cf95303fed567e42237e73b1e316567327629554
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-18 12:39:06 +0700

    adde title, likes adn person dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <div className="flex justify-center items-center h-screen">Welcome to the jungle</div>;
+  const title = "welcome to the new world 0.4 not 4.0 in my country";
+  const likes = 50;
+  const person = { name: "yoshi", age: 24 };
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center h-screen">
+      <h1>{title}</h1>
+      <p>Liked {likes}</p>
+      <h3>{person.name}</h3>
+    </div>
+  );
 }
 
 export default App;
