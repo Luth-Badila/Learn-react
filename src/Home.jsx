99e5e2abfccc8be7ceab4084b087cce3f292d990
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-19 10:33:55 +0700

    added useState hook in home.js

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello, click me if you can ", e);
+  // let name = "Vegeta";
+  const [name, setName] = useState("Vegeta");
+  const [age, setAge] = useState(20);
+  const handleClick = () => {
+    setName("Goku");
+    setAge(30);
   };
 
-  const handleClickAgain = (name, e) => {
-    console.log("Please, don't click me " + name, e.target);
-  };
+  // const handleClickAgain = (name, e) => {
+  //   console.log("Please, don't click me " + name, e.target);
+  // };
 
   return (
-    <div className="flex flex-col p-8 mt-3 ">
+    <div className="flex flex-col p-8 mt-3 gap-8">
       <h1>Home</h1>
+      <p>{name}</p>
+      <p>{age}</p>
       <button onClick={handleClick} className="flex justify-start items-center bg-blue-700 p-2 w-[80px] rounded-lg">
         Click Me
-      </button>
-      <button onClick={(e) => handleClickAgain("Mario", e)} className="flex justify-start items-center bg-blue-700 p-2 w-[120px] rounded-lg">
-        Click me again
       </button>
     </div>
   );
