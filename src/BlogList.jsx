9496435041fad49ffef25645098acc7c481d5aa4
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-20 10:57:46 +0700

    added loading message

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="flex flex-col p-8 mt-3 gap-8">
       <h1 className="text-xl font-bold">{title}</h1>
@@ -8,9 +8,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="p-8 hover:shadow-lg hover:shadow-blue-500/50" key={blog.id}>
           <h1 className="text-xl font-bold">{blog.title}</h1>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)} className="p-2 bg-blue-700 text-white rounded-md">
-            Delete Blogs
-          </button>
         </div>
       ))}
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,24 +2,24 @@ import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My New Website", body: "Lorem ipsum dolor....", author: "Vegeta", id: 1 },
-    { title: "Welcome Party !!!", body: "Lorem ipsum dolor....", author: "Goku", id: 2 },
-    { title: "Web Dev Top Tips", body: "Lorem ipsum dolor....", author: "Krilin", id: 3 },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    console.log("Im run away from you");
-  }, [name]);
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs").then((res) => {
+        return res.json().then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+        });
+      });
+    }, 1000);
+  }, []);
 
   return (
     <div className="flex flex-col p-8 mt-3 gap-8">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {isLoading && <div>Now Loading .....</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
