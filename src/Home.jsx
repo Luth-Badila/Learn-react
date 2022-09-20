548354636b733ba416638c0fac3bd9251e971705
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-20 09:52:22 +0700

    added dependencies useEffect using nam and setName

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
     { title: "Web Dev Top Tips", body: "Lorem ipsum dolor....", author: "Krilin", id: 3 },
   ]);
 
+  const [name, setName] = useState("Goku");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -15,13 +17,17 @@ const Home = () => {
 
   useEffect(() => {
     console.log("Im run away from you");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="flex flex-col p-8 mt-3 gap-8">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === "Goku")} title="Goku's Blogs!" />
+      <button className="w-[120px] p-2 bg-blue-700 text-white rounded-md" onClick={() => setName("Bejita")}>
+        change name
+      </button>
+      <p>{name}</p>
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Goku")} title="Goku's Blogs!" /> */}
     </div>
   );
 };
