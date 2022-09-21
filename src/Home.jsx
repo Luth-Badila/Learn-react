d63faf9bcbc0d7fc4ada172fb652b6c744e5b35a
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-21 11:20:00 +0700

    added error handling

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,20 +4,34 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/blogs").then((res) => {
-        return res.json().then((data) => {
+      fetch("http://localhost:7000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
           setBlogs(data);
           setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
         });
-      });
     }, 1000);
   }, []);
 
+  //  npx json-server --watch data/db.json --port 7000
+
   return (
     <div className="flex flex-col p-8 mt-3 gap-8">
+      {error && <div>{error}</div>}
       {isLoading && <div>Now Loading .....</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
