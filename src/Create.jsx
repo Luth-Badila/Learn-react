58f689fb64c528fc2852ff073b8cb673532c2a6e
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-24 23:00:46 +0700

    made form in create.js

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Goku");
   return (
-    <div>
-      <div className="h-screen flex justify-center items-center">Create me</div>
-    </div>
+    <>
+      <div className="h-[80vh] flex flex-col justify-center items-center">
+        <h2 className="">Create me</h2>
+        <form className="flex flex-col gap-3 w-[400px]">
+          <label className="text-[20px]">Blog title</label>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required className="h-[50px] border border-black p-2" />
+          <label className="text-[20px]">Blog body</label>
+          <textarea required value={body} onChange={(e) => setBody(e.target.value)} className="h-[80px] border border-black p-2" />
+          <label className="text-[20px]">Blog author</label>
+          <select className="p-2" value={author} onChange={(e) => setAuthor(e.target.value)}>
+            <option value="Goku">Goku</option>
+            <option value="Bejita">Bejita</option>
+          </select>
+          <button className="bg-blue-700 text-white rounded-md cursor-pointer w-[100px]">Add Blog</button>
+          <p>{title}</p>
+          <p>{body}</p>
+          <p>{author}</p>
+        </form>
+      </div>
+    </>
   );
 };
 
