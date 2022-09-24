800a7b4dbcf49267260184342d5b70d855f86e6d
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-09-24 10:43:30 +0700

    added data in blog detail that from usefetch

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, isLoading, error } = useFetch("http://localhost:7000/blogs/" + id);
 
   return (
     <>
-      <h1 className="font-bold text-3xl">BlogDetails - {id} </h1>;
+      <h1 className="flex flex-col p-8 mt-3 gap-8 text-3xl">
+        {isLoading && <div>Now Loading.....</div>}
+        {error && <div>{error}</div>}
+        {blog && (
+          <article>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <div>{blog.body}</div>
+          </article>
+        )}
+      </h1>
+      ;
     </>
   );
 };
